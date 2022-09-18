9d1e63dc4e8fc65dfc12ff3151f567d02d1daab5
Samuel Suarez <sdsuarez@saludelectronica.com>
2022-09-17 23:49:03 -0500

    inicio con contraseña

--- src/components/buttonModalComponent.js
+++ src/components/buttonModalComponent.js
@@ -1,29 +1,38 @@
-import React,{useState} from 'react'
-import Button from 'react-bootstrap/Button';
-import ModalBodyComponent from './modalBodyComponent';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import ModalBodyComponent from "./modalBodyComponent";
 
 const ButtonModalComponent = (props) => {
-    
   const [modalShow, setModalShow] = useState(false);
 
   const returnUrlHome = () => {
-    const url = window.location.href.replace('/tableAppoiment',"/tarotapp")
-    window.location.replace(url)
-  }
+    const url = window.location.href.replace("/tableAppoiment", "/tarotapp");
+    window.location.replace(url);
+  };
 
   return (
-      <>
-            <Button variant="secondary" onClick={() =>  window.location.href.includes("/tarotapp") ? setModalShow(true) : returnUrlHome() }>
-                {props.titleButtonModal}
-            </Button>
+    <>
+      <Button
+        variant="secondary"
+        onClick={() =>
+          window.location.href.includes("/tarotapp")
+            ? setModalShow(true)
+            : returnUrlHome()
+        }
+      >
+        {props.titleButtonModal}
+      </Button>
 
-            <ModalBodyComponent
-            titlemodal={props.titlemodal}
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-            > {props.children} </ModalBodyComponent>
-      </>
-  )
-}
+      <ModalBodyComponent
+        titlemodal={props.titlemodal}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      >
+        {" "}
+        {props.children}{" "}
+      </ModalBodyComponent>
+    </>
+  );
+};
 
-export default ButtonModalComponent
+export default ButtonModalComponent;
--- src/components/formLoginComponent.js
+++ src/components/formLoginComponent.js
@@ -1,21 +1,46 @@
-import React from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useLocation } from 'react-router-dom'
+import { toast } from "react-toastify";
+import { db } from "../database/firebase";
 
 const FormLoginComponent = () => {
   const location = useLocation();
+  const [pass, setPass] = useState("")
   const validateLoginFrom = (event) => {
     event.preventDefault()
-    const url = window.location.href.replace("/tarotapp", '/tableAppoiment')
-    window.location.replace(url)
+    const md5 = require('md5')
+    const passE = md5(pass)
+    try {
+      const q = query(
+        collection(db, "pass"),
+        where("passE", "==", passE)
+      );
+      getDocs(q).then(async (rsp) => {
+        if (rsp && rsp.empty) {
+          toast.error("Contraseña incorrecta")
+        } else {
+          const url = window.location.href.replace("/tarotapp", '/tableAppoiment')
+          window.location.replace(url)
+        }
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  const handlePassword = (event) => {
+    const {value} = event.target
+    if(value && value != "") setPass(value)
   }
 
   return (
     <Form onSubmit={validateLoginFrom}>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Contraseña" />
+        <Form.Control type="password" placeholder="Contraseña" required={true} onChange={(event) => handlePassword(event)}/>
       </Form.Group>
       <div className="d-grid gap-2">
         <Button variant="secondary" type="submit">
