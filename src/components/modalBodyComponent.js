893b922548c2c2aa1aefc3698a4ee207bc64027c
Samuel Suarez <sdsuarez@saludelectronica.com>
2022-09-18 17:55:08 -0500

    estilos

--- src/components/modalBodyComponent.js
+++ src/components/modalBodyComponent.js
@@ -10,15 +10,15 @@ const ModalBodyComponent = (props) => {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
+      <Modal.Header closeButton style={{backgroundColor:'#232f49', color: 'white', margin: '0px'}}>
         <Modal.Title id="contained-modal-title-vcenter">
-          {props.titlemodal}
+          {props.titlemodal ? props.titlemodal : 'Ingresar'}
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body style={{backgroundColor:'#232f49', color: 'white'}}>
           {props.children}
       </Modal.Body>
-      <Modal.Footer>
+      <Modal.Footer style={{backgroundColor:'#232f49', color: 'white'}}>
         <Button variant="danger" onClick={props.onHide}>Cerrar</Button>
       </Modal.Footer>
     </Modal>
