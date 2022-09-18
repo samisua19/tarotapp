48423a8e99fe7a9827383701afdafa912195b1d9
Samuel Suarez <sdsuarez@saludelectronica.com>
2022-09-17 22:40:10 -0500

    tabla

--- src/components/tableAppoimentComponent.js
+++ src/components/tableAppoimentComponent.js
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
-import { Row, Col, Card, Container, Button, Image } from 'react-bootstrap'
+import { Container, Button, Table } from "react-bootstrap";
 import { db } from "../database/firebase";
 
 const TableAppoimentComponent = () => {
   const [appoiment, setAppoiment] = useState([]);
   useEffect(() => {
-      const addParseObjet = async (querySnapshot) => {
-          const arrAppoiment = []
-          querySnapshot.forEach(appoiment => {
-              arrAppoiment.push(appoiment.data())
-          });
-          return arrAppoiment
-      }
+    const addParseObjet = async (querySnapshot) => {
+      const arrAppoiment = [];
+      querySnapshot.forEach((appoiment) => {
+        arrAppoiment.push(appoiment.data());
+      });
+      return arrAppoiment;
+    };
     const getAppoiment = async () => {
       onSnapshot(collection(db, "appoiment"), async (querySnapshot) => {
         const arrAppoiment = await addParseObjet(querySnapshot);
         setTimeout(() => {
-            console.log(arrAppoiment)
+          console.log(arrAppoiment);
           setAppoiment(arrAppoiment);
         }, 1000);
       });
@@ -29,17 +29,34 @@ const TableAppoimentComponent = () => {
     <div className="App-header" style={{ justifyContent: "flex-start" }}>
       <h1>Agendas</h1>
       <Container>
-        <Row xs={1} md={4} className="g-2">
-          {Object.values(appoiment)
-            .sort(($a, $b) => $b.lastModified - $a.lastModified)
-            .map((_, idx) => {
+        <Table striped responsive bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Fecha cita</th>
+              <th>Hora cita</th>
+              <th>Correo</th>{/* 
+              <th>Estado</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {Object.values(appoiment).map((_, idx) => {
               return (
-                <Col key={_.id}>
-                    <h1>{_.name}</h1>
-                </Col>
+                <tr key={_.id}>
+                  <td>{_.name}</td>
+                  <td>{_.appoimentDate}</td>
+                  <td>{_.appoimentHour}</td>
+                  <td>{_.email}</td>
+                  {/* <td>
+                    <Button onClick={async () => await activeOrInactiveGirl(_)}>
+                      {_.active ? "Activa" : "Inactiva"}
+                    </Button>
+                  </td> */}
+                </tr>
               );
             })}
-        </Row>
+          </tbody>
+        </Table>
       </Container>
     </div>
   );
