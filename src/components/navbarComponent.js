586bee26b455b4b19394be52817431cd8ccf1114
Samuel Suarez <sdsuarez@saludelectronica.com>
2022-10-19 22:48:00 -0500

    responsive

--- src/components/navbarComponent.js
+++ src/components/navbarComponent.js
@@ -6,74 +6,79 @@ import FormLoginComponent from "./formLoginComponent";
 const NavbarComponent = (props) => {
   return (
     <Navbar className="navBar">
-      <Row style={{ display: "flex" }} className="navBarContent">
-        <Col style={{ display: "flex" }}>
-          <Image
-            src={process.env.PUBLIC_URL + "/logotarot.png"}
-            className="logoNavbar"
-          />
-          <div
+      <Container>
+        <Row
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+          className="navBarContent"
+        >
+          <Col style={{ display: "flex" }} xl={8}>
+            <Image
+              src={process.env.PUBLIC_URL + "/logotarot.png"}
+              className="logoNavbar"
+            />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <h1
+                style={{
+                  margin: "0px",
+                  color: "white",
+                  fontFamily: ["Comic Sans MS"],
+                }}
+              >
+                Agenda Espiritual
+              </h1>
+            </div>
+          </Col>
+          <Col
+            xl={4}
+            className="containerPhone"
             style={{
               display: "flex",
               flexDirection: "column",
-              justifyContent: "center",
+              backgroundColor: "#b38fc6",
+              height: "100px",
+              borderRadius: "10px",
+              padding: "7px",
             }}
           >
-            <h1
-              style={{
-                margin: "0px",
-                color: "white",
-                fontFamily: ["Comic Sans MS"],
-              }}
-            >
-              Agenda Espiritual
-            </h1>
-          </div>
-        </Col>
-        <Col
-          className="containerPhone"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            backgroundColor: "#b38fc6",
-            height: "100px",
-            borderRadius: "10px",
-            padding: "7px",
-          }}
-        >
-          <Row style={{display: 'flex', flexDirection: 'column'}}>
-            <Col>
-              <h3 style={{ color: "white", fontFamily: ["Comic Sans MS"] }}>
-                Número de Contacto:{" "}
-              </h3>
-            </Col>
-            <Col style={{ display: "flex" }}>
-              <Row >
-                <Col xs={2}>
-                  <Image
-                    src={process.env.PUBLIC_URL + "/logowha.png"}
-                    height="30"
-                    style={{ borderRadius: "20px" }}
-                  />
-                </Col>
-                <Col>
-                  <a href="#">
-                    <h3 style={{ fontFamily: ["Comic Sans MS"] }}>
-                      0124 181241 23423
-                    </h3>
-                  </a>
-                </Col>
-
-              </Row>
-              <div
-                
-              >
-              </div>
-              
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+            <Row style={{ display: "flex", flexDirection: "column" }} xl={12}>
+              <Col xl={12}>
+                <h3 style={{ color: "white", fontFamily: ["Comic Sans MS"] }}>
+                  Número de Contacto:
+                </h3>
+              </Col>
+              <Col style={{ display: "flex" }} xl={12}>
+                <Row>
+                  <Col xl={2} md={2} sm={2} xs={2}>
+                    <Image
+                      src={process.env.PUBLIC_URL + "/logowha.png"}
+                      height="30"
+                      style={{ borderRadius: "20px" }}
+                    />
+                  </Col>
+                  <Col xl={10} md={10} sm={10} xs={10}>
+                    <a href="#">
+                      <h3 style={{ fontFamily: ["Comic Sans MS"] }}>
+                        0124 181241 234
+                      </h3>
+                    </a>
+                  </Col>
+                </Row>
+                <div></div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </Navbar>
   );
 };
